dd1c9d2493994f06f2e23fdc0908e9aed774c416
Marcondes <bshoopkss@gmail.com>
2022-05-06 17:41:15 -0300

    littlechange

--- src/components/WindgetForm/Steps/ScreenshotButton.tsx
+++ src/components/WindgetForm/Steps/ScreenshotButton.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import html2canvas from 'html2canvas';
 import { Loading } from "../../Loading";
 
 interface ScreenshotButtonProps {
-  onScreenshotTook: (Screenshot: string) => void;
+  screenshot: string | null;
+  onScreenshotTook: (Screenshot: string | null) => void;
 }
 
-export function ScreenshotButton({ onScreenshotTook }: ScreenshotButtonProps) {
+export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
   async function handleTakeScreenshot() {
     setIsTakingScreenshot(true);
@@ -17,6 +18,16 @@ export function ScreenshotButton({ onScreenshotTook }: ScreenshotButtonProps) {
     setIsTakingScreenshot(false);
   }
 
+  if(screenshot) {
+    return (
+      <button type="button" className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors" onClick={()=> onScreenshotTook(null)} style={{   backgroundImage: `url(${screenshot})`,
+        backgroundPosition: 'right bottom',
+        backgroundSize: 180,
+      }}>
+        <Trash weight="fill"/>
+      </button>
+    )
+  }
 
   return (
     <button onClick={handleTakeScreenshot} className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500" type="button">
